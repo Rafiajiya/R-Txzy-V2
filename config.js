384012f5cc5847070be9c8e8af510129f2d98ae5
AdeurX <98345064+AdeurX@users.noreply.github.com>
2022-02-18 15:50:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.APIKeys = { // APIKey nya disini
 global.packname = 'shiro'
 global.author = 'loppyuuu<3'
 
-global.wait = '「 ⏱️ 」Harap tunggu...'
+global.wait = 'Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '©Shiro Botz' //change the watermark 
